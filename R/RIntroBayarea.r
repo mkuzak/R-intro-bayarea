ff2bec259bc7fa84ff4da0bad23fef95c892abbd
mkuzak <mateusz.kuzak@gmail.com>
2015-02-03 11:27:42 +0100

    Laod and start cleaning data

---
+++ R/RIntroBayarea.r
@@ -0,0 +1,28 @@
+# read in house sales data
+
+sales_file <- system.file('extdata', 'house-sales.csv', package='R-intro-bayarea' )
+sales <- read.csv(sales_file, stringsAsFactors=FALSE)
+
+# read in geolocation data
+ad_file <- system.file('extdata', 'addresses.csv', package='R-intro-bayarea' )
+ad <- read.csv(ad_file, stringsAsFactors=FALSE)
+
+# by default everything is read in as strings
+# we need to convert date strings into date objects
+sales$date <- as.POSIXct(strptime(sales$date, '%Y-%m-%d'))
+
+# and prices into numeric values
+sales$price <- as.numeric(sales$price)
+
+# check if there are missing vaules in sales or date
+any(is.na(sales$price))
+any(is.na(sales$date))
+
+# remove missing rows with missing dates
+sales <- sales[!is.na(sales$date), ]
+
+# combine sales data with geospatial information
+names(sales)
+names(ad)
+
+geo <- join(sales, ad, by = c())
