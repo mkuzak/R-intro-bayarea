8179b713ab55015630a943583b6f3e5feeff8412
mkuzak <mateusz.kuzak@gmail.com>
2015-02-04 15:09:29 +0100

    Overview of the dataset

--- R/RIntroBayarea.r
+++ R/RIntroBayarea.r
@@ -1,10 +1,13 @@
-# read in house sales data
+library(plyr)
+library(ggplot2)
 
-sales_file <- system.file('inst/extdata', 'house-sales.csv', package='RInroBayarea' )
+# read in house sales data
+path = system.file(package='RIntroBayarea')
+sales_file <- paste0(path, "/extdata/house-sales.csv")
 sales <- read.csv(sales_file, stringsAsFactors=FALSE)
 
 # read in geolocation data
-ad_file <- system.file('inst', 'extdata', 'addresses.csv', package='RIntroBayarea' )
+ad_file <- paste0(path, '/extdata/addresses.csv')
 ad <- read.csv(ad_file, stringsAsFactors=FALSE)
 
 # by default everything is read in as strings
@@ -25,4 +28,31 @@ sales <- sales[!is.na(sales$date), ]
 names(sales)
 names(ad)
 
-geo <- join(sales, ad, by = c())
+# by common columns
+intersect_cols <- intersect(names(sales), names(ad))
+geo <- join(sales, ad, by = intersect_cols)
+
+# choose only records with goog quality geocoding
+precise_qual <- c(
+  "QUALITY_ADDRESS_RANGE_INTERPOLATION", "QUALITY_EXACT_PARCEL_CENTROID",
+  "gpsvisualizer")
+precise <- subset(geo, quality %in% precise_qual)
+
+# choose cities with at least 20 sales a week
+# how many weeks does our dataset cover?
+n_weeks <- as.integer((max(precise$date) - min(precise$date)) / 7)
+
+# calculate sales per city
+cities <- as.data.frame(table(precise$city))
+names(cities) <- c('city', 'freq')
+
+big_cities <- subset(cities, freq > n_weeks * 20)
+
+# see what we actually pick up
+ggplot(geo, aes(city)) +
+  geom_histogram() +
+  geom_hline(yintercept=n_weeks*20)
+
+# add interesting citie
+selected <- c(as.character(big_cities$city), 'Mountain View', 'Berkley')
+bigc_geo <- subset(geo, city %in% selected)
